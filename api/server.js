dff19a68602ca2e521106cb9ea558ae58e36a693
elijahud <udomelijah@protonmail.com>
2026-06-25 18:00:18 +0100

    removed api

--- api/server.js
+++
@@ -1,7 +0,0 @@
-export const config = { runtime: "edge" };
-
-export default async function handler(request) {
-  const mod = await import("../dist/server/server.js");
-  const h = mod.default ?? mod;
-  return h.fetch(request);
-}
